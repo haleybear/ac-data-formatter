3c6e736164cb47515822771c8e68b6946688def2
Haley Bear <haley@haleybear.com>
2020-04-26 20:51:43 -0400

    Input file is read and Output file is created. Does not handle multiple instances of delimiter well, yet.

--- Desktop/development/ac-data-formatter/acrawformatting.py
+++ Desktop/development/ac-data-formatter/acrawformatting.py
@@ -1,21 +1,33 @@
-import json
+import sys, json
 
+if len(sys.argv)<2:
+    print('Fatal: Please include an input and output file.')
+    print("Usage: Python3 %s <input file name> <output file name>" % sys.argv[0])
+    sys.exit(1)
+
+filename = sys.argv[1] #input source file
+outputfilename = sys.argv[2] #output source file
 fields = ['Name','Price','Location','Time','Months']
-filename = input()
-outputfilename = input()
 output_file = open(outputfilename,'w')
-formatlist = []
-outputdict = {}
+final_result = []
+obj = {}
+delimiting_string = " D"
+i = 0
 
+with open(filename) as f:
+    for line in f:
+        if delimiting_string in line:
+            #reset counter
+            #check if in next line too - if so, skip and move on
 
-with open(filename) as fh:
-    i = 0
-    for line in fh:
-        if i == 0:
-            outputdict[fields[i]] = line
-            i += 1
-        else if i == 1:
+            i = 0
+            obj[fields[4]]=' '
+            final_result.append(obj)
+            obj = dict()
+            continue
+        cleanup = line.rstrip("\n\r")
+        obj[fields[i]] = cleanup
+        i += 1
 
-        while i<len(fields):
-            [fields[i]] = description[i]
-
+with open(outputfilename, 'w', encoding='utf-8') as f:
+    f.write(json.dumps(final_result, ensure_ascii=False))
